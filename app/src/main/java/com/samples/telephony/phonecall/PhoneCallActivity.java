5805b0a1e752f2d4fdfe2e1ae576ecdd6902a0af
java2019 <java_2019@mail.ru>
2017-06-26 11:41:03 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/telephony/phonecall/PhoneCallActivity.java
@@ -0,0 +1,13 @@
+package com.samples.telephony.phonecall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneCallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_call);
+    }
+}
